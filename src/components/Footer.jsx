0b3be835ddfef5102ef369159875e972943da9ae
luismagnum <luismagnum1@gmail.com>
2023-10-15 00:33:25 -0300

    responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='relative'>
     <div className='absolute top-0 left-0 w-full overflow-hidden bg-zinc-900'>
-    <div className='grid lg:grid-cols-4 ml-24 md:grid-cols-2 grip-col-1 p-20 m-4'>
+    <div className='grid lg:grid-cols-4 ml-1 md:grid-cols-2 grip-col-1 p-20 m-4'>
         <div className='flex flex-col gap-5'>
          <h2 className='text-[22px] text-yellow-600 gap-5 font-semibold py-2 uppercase'>Empanadas</h2>
          <ul>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,9 @@ const Navbar = () => {
       <img className ='w-14 ml-5' src={img} alt=''/>
       <span className='nav-logo'>Sabores de Venezuela</span>
       <div className={`nav-items ${isOpen && "open"}`}>
-      <i class="fa-solid fa-house"></i>
         <a href='/home'>Home</a>
-        <i class="fa-sharp fa-solid fa-users"></i>
-        <a href='/Info'>Comidas</a>
-        <i class="fa-brands fa-servicestack"></i>
-        <a href='/Product'>Combos</a>
-        <i class="fa-sharp fa-solid fa-address-book"></i>
+        <a href='#Seccion1'>Comidas</a>
+        <a href='#shop'>Combos</a>
         <a href='/contact'>Contact</a>
       </div>
       <div className={`nav-toggle ${isOpen && "open"}`}
